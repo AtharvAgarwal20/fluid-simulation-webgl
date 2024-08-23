b60c24ba6e46ee8424b84bfc9f5fd6c781c3e97d
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-24 00:55:02 +0530

    colorful

--- components/FluidBackground/FluidBackground.tsx
+++ components/FluidBackground/FluidBackground.tsx
@@ -1,52 +1,55 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
-import webGLFluidEnhanced from 'webgl-fluid-enhanced';
+import { useEffect, useRef } from "react";
+import webGLFluidEnhanced from "webgl-fluid-enhanced";
 
 export default function FluidBackground() {
-    const canvasRef = useRef(null)
-    useEffect(() => {
-        webGLFluidEnhanced.config({
-            SIM_RESOLUTION: 256, // Resolution of the simulation grid
-            DYE_RESOLUTION: 2048, // Resolution of the dye grid
-            CAPTURE_RESOLUTION: 512, // Resolution of captured frames
-            DENSITY_DISSIPATION: 1, // Rate at which density dissipates
-            VELOCITY_DISSIPATION: 0.2, // Rate at which velocity dissipates
-            PRESSURE: 0.8, // Pressure value used in the simulation
-            PRESSURE_ITERATIONS: 20, // Number of pressure iterations
-            CURL: 20, // Curl value used in the simulation
-            INITIAL: false, // Enables splats on initial load
-            SPLAT_AMOUNT: 5, // Number of initial splats (Random number between n and n * 5)
-            SPLAT_RADIUS: 0.25, // Radius of the splats
-            SPLAT_FORCE: 6000, // Force applied by the splats
-            SPLAT_KEY: 'Space', // Keyboard key to spawn new splats (empty to disable)
-            SHADING: true, // Enables shading in the visualization
-            COLORFUL: true, // Enables rapid changing of colors
-            COLOR_UPDATE_SPEED: 10, // Speed of color update
-            COLOR_PALETTE: ['#FF1D8E'], // Custom color palette (empty by default, uses hex colors)
-            HOVER: true, // Enables interaction on hover
-            BACK_COLOR: '#000', // Background color of the canvas
-            TRANSPARENT: false, // Makes the canvas transparent if true
-            BRIGHTNESS: 0.5, // Color brightness (Recommend lower than 1.0 if BLOOM is true)
-            BLOOM: true, // Enables bloom effect
-            BLOOM_ITERATIONS: 8, // Number of bloom effect iterations
-            BLOOM_RESOLUTION: 256, // Resolution of the bloom effect
-            BLOOM_INTENSITY: 0.2, // Intensity of the bloom effect
-            BLOOM_THRESHOLD: 0.5, // Threshold for the bloom effect
-            BLOOM_SOFT_KNEE: 0.5, // Soft knee value for the bloom effect
-            SUNRAYS: true, // Enables sunrays effect
-            SUNRAYS_RESOLUTION: 196, // Resolution of the sunrays effect
-            SUNRAYS_WEIGHT: 0.5, // Weight of the sunrays effect
-        });
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    webGLFluidEnhanced.config({
+      SIM_RESOLUTION: 256, // Resolution of the simulation grid
+      DYE_RESOLUTION: 2048, // Resolution of the dye grid
+      CAPTURE_RESOLUTION: 512, // Resolution of captured frames
+      DENSITY_DISSIPATION: 1, // Rate at which density dissipates
+      VELOCITY_DISSIPATION: 0.2, // Rate at which velocity dissipates
+      PRESSURE: 0.8, // Pressure value used in the simulation
+      PRESSURE_ITERATIONS: 20, // Number of pressure iterations
+      CURL: 20, // Curl value used in the simulation
+      INITIAL: false, // Enables splats on initial load
+      SPLAT_AMOUNT: 5, // Number of initial splats (Random number between n and n * 5)
+      SPLAT_RADIUS: 0.25, // Radius of the splats
+      SPLAT_FORCE: 6000, // Force applied by the splats
+      SPLAT_KEY: "Space", // Keyboard key to spawn new splats (empty to disable)
+      SHADING: true, // Enables shading in the visualization
+      COLORFUL: true, // Enables rapid changing of colors
+      COLOR_UPDATE_SPEED: 10, // Speed of color update
+      COLOR_PALETTE: [], // Custom color palette (empty by default, uses hex colors)
+      HOVER: true, // Enables interaction on hover
+      BACK_COLOR: "#000", // Background color of the canvas
+      TRANSPARENT: false, // Makes the canvas transparent if true
+      BRIGHTNESS: 0.5, // Color brightness (Recommend lower than 1.0 if BLOOM is true)
+      BLOOM: true, // Enables bloom effect
+      BLOOM_ITERATIONS: 8, // Number of bloom effect iterations
+      BLOOM_RESOLUTION: 256, // Resolution of the bloom effect
+      BLOOM_INTENSITY: 0.2, // Intensity of the bloom effect
+      BLOOM_THRESHOLD: 0.5, // Threshold for the bloom effect
+      BLOOM_SOFT_KNEE: 0.5, // Soft knee value for the bloom effect
+      SUNRAYS: true, // Enables sunrays effect
+      SUNRAYS_RESOLUTION: 196, // Resolution of the sunrays effect
+      SUNRAYS_WEIGHT: 0.5, // Weight of the sunrays effect
+    });
 
-        webGLFluidEnhanced.simulation(canvasRef.current!);
-    }, [])
-    return (
-        <canvas style={{
-            width: '100vw',
-            height: '100dvh',
-            position: 'fixed',
-            top: 0
-        }} ref={canvasRef}></canvas>
-    )
+    webGLFluidEnhanced.simulation(canvasRef.current!);
+  }, []);
+  return (
+    <canvas
+      style={{
+        width: "100vw",
+        height: "100dvh",
+        position: "fixed",
+        top: 0,
+      }}
+      ref={canvasRef}
+    ></canvas>
+  );
 }
